c3b20591eb02c6f3a9759b2aab6e41576cd49bb1
Aleksandr <mrstuzer@gmail.com>
2025-10-03 16:40:38 +0600

    Create autoconf-vscode.sh

---
+++ autoconf-vscode.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+# Блокировка запуска от root
+[ "$EUID" -eq 0 ] && { echo "Не запускайте скрипт от root." >&2; exit 1; }
+
+# Проверка наличия sudo
+if sudo -l &>/dev/null; then
+  echo "✅ Есть права sudo"
+else
+  echo "❌ Нет прав sudo"
+  exit 1
+fi
+
+# Установка yay из AUR вручную, если не найден
+command -v yay &>/dev/null \
+  || {
+    echo "🔄 yay не найден — собираю из AUR..."
+    cd /tmp
+    [ -d yay ] && rm -rf yay
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay
+  }
+
+# Имя текущего пользователя
+USERNAME="$(whoami)"
+
+# Пути к файлам и папкам
+CONFIG_DIR="/home/$USERNAME/.config/Code/User"
+EXTENSIONS_DIR="/home/$USERNAME/.vscode/extensions"
+
+# Данные репозитория на GitHub
+REPO_OWNER="als-creator"
+REPO_NAME="autoconf-vscode"
+BRANCH="main"
+
+# URL ресурсов
+SETTINGS_JSON_URL="https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/${BRANCH}/settings.json"
+EXTENSIONS_URL="https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_NAME}/${BRANCH}/extensions.txt"
+
+# Установка Visual Studio Code через YAY
+echo "🚀 Начинаю установку Visual Studio Code..."
+sudo yay -Sy visual-studio-code-bin --noconfirm
+
+# Создание директории Config, если её нет
+mkdir -p "$CONFIG_DIR"
+
+# Скачивание настроек и помещение их в директорию пользователя
+wget -qO- "$SETTINGS_JSON_URL" > "$CONFIG_DIR/settings.json"
+
+# Установка расширений из списка
+EXTENSIONS=$(curl -s "$EXTENSIONS_URL")
+for ext in ${EXTENSIONS}; do
+  code --install-extension "$ext"
+done
+
+# Сообщение об успехе
+echo "✅ Настройки и расширения успешно установлены!"
+exit 0
